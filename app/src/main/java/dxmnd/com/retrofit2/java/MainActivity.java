408f1d294eec9de3fe79258353cd145ed5bc9838
KimHunJin <ysg01129@naver.com>
2018-02-24 04:20:53 +0900

    add comment

--- app/src/main/java/dxmnd/com/retrofit2/java/MainActivity.java
+++ app/src/main/java/dxmnd/com/retrofit2/java/MainActivity.java
@@ -42,23 +42,28 @@ public class MainActivity extends AppCompatActivity {
         map.put("password", "비밀번호");
 
         // 최상위 경로
+        // 이 부분에서 rx를 추가할 수도 있어요.
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://124.80.127.237:13000/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
+        // 서비스를 불러옵니다.
         RetrofitService service = retrofit.create(RetrofitService.class);
         Call<Model> call = service.getPost(map);
+
+        // 귀찮아서 일단은 로그로 찍었습니다.
         call.enqueue(new Callback<Model>() {
             @Override
             public void onResponse(@NonNull Call<Model> call, @NonNull Response<Model> response) {
                 Model model = response.body();
-                Log.e(TAG, "count : " + model.getCount());
-                int size = model.getResults().size();
+
+                Log.i(TAG, "count : " + model.getCount()); // 안전한 null 처리를 해주세요. 지금은 안했습니다.
+                int size = model.getResults().size(); // 마찬가지로 안전한 null 처리가 필요해요.
 
                 for (int i = 0; i < size; i++) {
-                    Log.e(TAG, "id : " + model.getResults().get(i).getId());
-                    Log.e(TAG, "name : " + model.getResults().get(i).getName());
+                    Log.i(TAG, "id : " + model.getResults().get(i).getId());
+                    Log.i(TAG, "name : " + model.getResults().get(i).getName());
                 }
             }
 
--- app/src/main/java/dxmnd/com/retrofit2/java/model/Item.java
+++ app/src/main/java/dxmnd/com/retrofit2/java/model/Item.java
@@ -2,6 +2,8 @@ package dxmnd.com.retrofit2.java.model;
 
 /**
  * Created by HunJin on 2018-02-24.
+ * 클래스 명을 제외한 나머지 이름의 명칭은 rest api와 통일시켜 주세요.
+ * 혹은 어노테이션을 사용해도 괜찮습니다. (GSON에서 제공할거에요 아마..)
  */
 
 public class Item {
